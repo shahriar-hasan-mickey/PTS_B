43d912c03d6c2c98660f38c22ea735f29e2a57aa
shahriar-hasan-mickey <shahriar.hasan.mickey@g.bracu.ac.bd>
2023-04-17 15:11:17 +0600

    5th Commit: included the build files (which were inside git ignore)

---
+++ app/build/generated/source/buildConfig/debug/humble/slave/pts_b/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package humble.slave.pts_b;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "humble.slave.pts_b";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
